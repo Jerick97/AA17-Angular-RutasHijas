243a34d71d45c7991b3dcc8ae7eeb1a098d9d4e0
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-12 11:44:04 -0500

    creación del header

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  icono:boolean=true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  Menu(){
+    this.icono=!this.icono;
+  }
+}

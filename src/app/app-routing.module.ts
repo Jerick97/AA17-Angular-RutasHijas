ac35350f9eb0d6749493508907aac86f52b47070
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-12 12:18:05 -0500

    Se añadio Rutas Hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MisionComponent } from './pages/mision/mision.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { VisionComponent } from './pages/vision/vision.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent}, //redireccionar al Home
-  {path: '**', pathMatch:'full',redirectTo:''}//Cualquier url redirija al Home
+  {path:'contacto',component:ContactoComponent},//redireccionar al Contacto
+  {path:'nosotros',component:NosotrosComponent, children:[
+    {path:'mision',component:MisionComponent}, //ruta hija Misión
+    {path:'vision',component:VisionComponent} //ruta hija Vision
+  ]}, //redireccionar al Nosotros
+  {path:'404',component:NotFoundComponent}, //redireccionar al Not Found
+  {path: '**', pathMatch:'full',redirectTo:'404'}//Cualquier url redirija al 404 Not Found
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MisionComponent } from './mision/mision.component';
+import { VisionComponent } from './vision/vision.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    NotFoundComponent,
+    MisionComponent,
+    VisionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    NotFoundComponent,
+    MisionComponent,
+    VisionComponent
   ]
 })
 export class PagesModule { }
